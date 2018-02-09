8afbebeb682ee7f8006d04c100edcc9559d794be
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-09 17:21:37 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      if @@items.any?{|i|i.name == item}
+        i = @@items.detect{|i|i.name == item}
+        resp.write "#{i.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end 
